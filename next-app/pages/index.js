dc2f09f7dc111b1a2b16fcdfe2a8f08b086689a7
roniDalmatinac <ognjenovic_sasa@hotmail.com>
2021-09-09 14:25:34 +0200

    next app

---
+++ next-app/pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      naslov 123
+    </header>
+
+
+
+
+
+
+
+
+    <div  className="tijelo"> 
+
+    
+
+
+      <img  src="https://www.greenqueen.com.hk/wp-content/uploads/2021/06/WEF-Investments-In-Nature-Based-Solutions-Have-To-Triple-By-2030-To-Address-Climate-Change-Biodiversity-Loss.jpg"   alt="Logo"  className="slika"/>
+
+      
+
+      
+     <img src="https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall-750x500.jpg"     alt="Logo" className="slika" />           
+     
+
+     
+     </div>
+    
+   
+ 
+    <footer className="dolje">
+      footer
+    </footer>
+
+ 
+  </div>
+  )
+}
